654c7ec0f89ca2e9f64aa03a18c808e9fb77fe11
kukorra <154669365+kukorra@users.noreply.github.com>
2025-03-31 22:21:06 +0500

    trylab.pl

--- trylab.pl
+++ trylab.pl
@@ -2,7 +2,7 @@ find_divisors(N, Divisors) :-
     findall(D, (between(1, N, D), N mod D =:= 0), Divisors).
 
 start :-
-    write('Ââåäèòå ÷èñëî: '),
+    write('Введите число: '),
     read(N),
     find_divisors(N, Divisors),
-    write('Äåëèòåëè: '), write(Divisors), nl.
+    write('Делители: '), write(Divisors), nl.
