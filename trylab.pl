202716bd4733a7da393d47d58799aca5f7ee295c
kukorra <154669365+kukorra@users.noreply.github.com>
2025-03-31 16:38:26 +0500

    Add files via upload

---
+++ trylab.pl
@@ -0,0 +1,9 @@
+find_divisors(N, Divisors) :-
+    findall(D, (between(1, N, D), N mod D =:= 0), Divisors).
+
+% Стартовая точка программы
+start :-
+    write('Введите число: '),
+    read(N),
+    find_divisors(N, Divisors),
+    write('Делители: '), write(Divisors), nl.
